e84c64177d73317afa0775a6b218aedabb45c4db
rackham <juanmanuelmg003@gmail.com>
2023-05-24 12:23:13 +0200

    canvis en el input update

--- src/Main.java
+++ src/Main.java
@@ -74,7 +74,7 @@ public class Main {
         m.select();
         //m.insert("aaa");
         //m.select();
-        m.update(0,"aloh");
+        m.update(0,"caracol");
         //m.select();
         m.delete(20);
         m.close();
